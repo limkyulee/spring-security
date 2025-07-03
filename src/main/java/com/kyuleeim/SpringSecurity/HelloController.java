c06977871f3e0012a3cb781c6f7379f5b11dd0b2
limkyulee <mud0308@gmail.com>
2025-07-03 09:59:33 +0900

    Initial Commit

---
+++ src/main/java/com/kyuleeim/SpringSecurity/HelloController.java
@@ -0,0 +1,19 @@
+package com.kyuleeim.SpringSecurity;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author limkyulee
+ * @version 1.0, 7/3/25
+ * @see {참조}
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello(HttpServletRequest request) {
+        return "Hello World!" + request.getSession().getId();
+    }
+}
