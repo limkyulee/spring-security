a4fd566ece2009d1a930d0ef98839ab053f1edfd
limkyulee <mud0308@gmail.com>
2025-07-04 09:18:52 +0900

    csrf token

--- src/main/java/com/kyuleeim/SpringSecurity/StudentController.java
+++ src/main/java/com/kyuleeim/SpringSecurity/StudentController.java
@@ -1,7 +1,9 @@
 package com.kyuleeim.SpringSecurity;
 
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,10 +27,14 @@ public class StudentController {
         return students;
     }
 
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
     @PostMapping("/students")
     public Student addStudent(@RequestBody Student student) {
         students.add(student);
-
         return student;
     }
 }
