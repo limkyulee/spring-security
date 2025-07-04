31bcefc52d27edc24627b9c144dfebbb400bb7f2
limkyulee <mud0308@gmail.com>
2025-07-04 15:37:03 +0900

    user hardcoding for test

--- src/main/java/com/kyuleeim/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/com/kyuleeim/SpringSecurity/config/SecurityConfig.java
@@ -5,7 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -19,18 +24,46 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http.csrf(customizer -> customizer.disable());
-        // @see 인증 없이 페이지 접근 불가
-        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-        // Default 로그인 페이지로 로그인
-//        http.formLogin(Customizer.withDefaults());
-        // 페이지 내용을 결과값으로 받음
-        http.httpBasic(Customizer.withDefaults());
-        // 세션을 사용하지 않음 (매 요청마다 인증처리)
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        return http.build();
+         /*
+         @flow
+         - csrf 셋팅 제거
+         - 인증 없이 페이지 접근 불가
+         - 페이지 내용을 결과값으로 받음
+         - 세션을 사용하지 않음 (매 요청마다 인증처리)
+         - 적용
+         */
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session ->
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 
+    /**
+     * @method userDetailService
+     * @description user hardcoding
+     * @return
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user1 = User
+            .withDefaultPasswordEncoder()
+            .username("kyuleelim01")
+            .password("springsecurity")
+            .roles("USER")
+            .build();
+
+
+        UserDetails user2 = User
+            .withDefaultPasswordEncoder()
+            .username("kyuleelim02")
+            .password("springsecurity")
+            .roles("ADMIN")
+            .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
 }
