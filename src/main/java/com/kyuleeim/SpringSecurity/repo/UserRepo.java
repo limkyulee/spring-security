b0076b676fcd2536d1a6cd73d6a50f9fa7824300
kyuleelim <mud0308@gmail.com>
2025-07-08 23:35:33 +0900

    connect postgres

--- src/main/java/com/kyuleeim/SpringSecurity/repo/UserRepo.java
+++ src/main/java/com/kyuleeim/SpringSecurity/repo/UserRepo.java
@@ -1,11 +1,17 @@
 package com.kyuleeim.SpringSecurity.repo;
 
+import com.kyuleeim.SpringSecurity.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author limkyulee
  * @version 1.0, 7/4/25
  * @see {참조}
  */
-public interface UserRepo extends JpaRepository<User, Integer> {
+
+@Repository
+public interface UserRepo extends JpaRepository<Users, Integer> {
+
+    Users findByUsername(String username);
 }
--- src/main/java/com/kyuleeim/SpringSecurity/service/MyUserDetailsService.java
+++ src/main/java/com/kyuleeim/SpringSecurity/service/MyUserDetailsService.java
@@ -1,5 +1,9 @@
 package com.kyuleeim.SpringSecurity.service;
 
+import com.kyuleeim.SpringSecurity.model.UserPrincipal;
+import com.kyuleeim.SpringSecurity.model.Users;
+import com.kyuleeim.SpringSecurity.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,8 +18,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private UserRepo userRepo;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
+        Users user = userRepo.findByUsername(username);
+
+        if(user == null) {
+            System.out.println("user not found");
+            throw new UsernameNotFoundException("user not found");
+        }
+
+        return new UserPrincipal(user);
     }
 }
