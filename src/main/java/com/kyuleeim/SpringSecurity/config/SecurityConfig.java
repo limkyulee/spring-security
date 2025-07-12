6c11942a587753cea69096706db73d7f644c412f
kyuleelim <mud0308@gmail.com>
2025-07-12 23:07:17 +0900

    spring security

--- src/main/java/com/kyuleeim/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/com/kyuleeim/SpringSecurity/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -30,16 +31,15 @@ public class SecurityConfig {
     UserDetailsService userDetailsService;
 
     /**
-     * @method securityFilterChain
-     * @flow
-     *   - csrf 셋팅 제거
-     *   - 인증 없이 페이지 접근 불가
-     *   - 페이지 내용을 결과값으로 받음
-     *   - 세션을 사용하지 않음 (매 요청마다 인증처리)
-     *   - 적용
      * @param http
      * @return SecurityFilterChain
      * @throws Exception
+     * @method securityFilterChain
+     * @flow - csrf 셋팅 제거
+     * - 인증 없이 페이지 접근 불가
+     * - 페이지 내용을 결과값으로 받음
+     * - 세션을 사용하지 않음 (매 요청마다 인증처리)
+     * - 적용
      */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -48,46 +48,20 @@ public class SecurityConfig {
                 .authorizeHttpRequests(request -> request.anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session ->
-                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
 
     /**
+     * @return
      * @method authenticationProvider
      * @description 사용자 인증 처리 담당 provider 등록
-     * @return
      */
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
         provider.setUserDetailsService(userDetailsService);
         return provider;
     }
-
-//    /**
-//     * @method userDetailService
-//     * @description user hardcoding
-//     * @return
-//     */
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//
-//        UserDetails user1 = User
-//            .withDefaultPasswordEncoder()
-//            .username("kyuleelim01")
-//            .password("springsecurity")
-//            .roles("USER")
-//            .build();
-//
-//
-//        UserDetails user2 = User
-//            .withDefaultPasswordEncoder()
-//            .username("kyuleelim02")
-//            .password("springsecurity")
-//            .roles("ADMIN")
-//            .build();
-//
-//        return new InMemoryUserDetailsManager(user1, user2);
-//    }
 }
---
+++ src/main/java/com/kyuleeim/SpringSecurity/service/UserService.java
@@ -0,0 +1,21 @@
+package com.kyuleeim.SpringSecurity.service;
+
+import com.kyuleeim.SpringSecurity.model.Users;
+import com.kyuleeim.SpringSecurity.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
+
+    public Users register(Users user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        return userRepo.save(user);
+    }
+}
